4933900176e81d937ab9ce289e6bacda558f12f8
Michael Bruno <michael.bruno1337@gmail.com>
2019-04-07 14:08:16 -0700

    Minor deploy script change

--- deploy.js
+++ deploy.js
@@ -4,13 +4,13 @@ const ghPages = require('gh-pages')
 
 
 const indexPath = path.join(__dirname, './dist/index.html')
-fs.readFile(indexPath, (err, data) => {
+fs.readFile(indexPath, 'utf8', (err, data) => {
   if (err) throw err;
+
   const result = data.replace(/\<base href=\"\/\"\>/, '<base href="/critical-failure/">')
   fs.writeFile(indexPath, result, 'utf8', function (err) {
     if (err) return console.log(err);
   });
 })
-
 
 ghPages.publish('dist');
